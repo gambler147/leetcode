1bb23c5a88c96beafffa69c6f73a8364b5b92aee
gambler147 <zhengyi.fang95@gmail.com>
2021-04-18 12:26:32 -0400

    removed old folders

--- biweeklycontest47/CheckIfNumberIsASumOfPowersOfThree/Solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def checkPowersOfThree(self, n: int) -> bool:
-        while n > 0:
-            q, r = n//3, n%3
-            if r > 1:
-                return False
-            n = q
-        return True
-    
