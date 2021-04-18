4d0272e57f77f070a207b1c7273ad60709252bab
gambler147 <zhengyi.fang95@gmail.com>
2021-04-18 12:25:08 -0400

    remove older folder

--- weeklycontest231/MinimumElementsToAddToFormAGivenSum/Solution.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def minElements(self, nums: List[int], limit: int, goal: int) -> int:
-        s = sum(nums)
-        return abs(goal - s) // limit + (abs(goal-s) % limit != 0)
-    
